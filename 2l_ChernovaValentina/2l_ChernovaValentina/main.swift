54a251a31b17172511201c2adcb48de97c215a1d
Lidiya666 <valeriya666@gmail.com>
2019-02-03 12:54:30 +0300

    2-й урок, надеюсь, верно все

---
+++ 2l_ChernovaValentina/2l_ChernovaValentina/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  2l_ChernovaValentina
+//
+//  Created by Lidiya on 03.02.2019.
+//  Copyright © 2019 lidiya. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
