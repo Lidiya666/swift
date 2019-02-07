e72c2af537b28c61831402855f56b022d8929900
Lidiya666 <valeriya666@gmail.com>
2019-02-07 22:07:36 +0300

    поменяла мутейтинг с одинаковым именем переменной

--- 3l_Chernova_Valentina.playground/2l_Chernova_Valentina.playground/main.swift
+++ 3l_Chernova_Valentina.playground/2l_Chernova_Valentina.playground/main.swift
@@ -28,8 +28,8 @@ struct Car {
     var isWindow: Window
     var isLuggage: Luggage
     
-    mutating func changeWindow(valWind: Window){
-        if valWind == Window.close {
+    mutating func changeWindow(isWindow: Window){
+        if self.isWindow == Window.close {
             self.isWindow = Window.open
         } else {
             self.isWindow = Window.close
@@ -41,7 +41,7 @@ var car1 = Car (type: .car, mark: "Hyundai", yearIssue: 2015, capLuggage: 510, i
 var car2 = Car (type: .car, mark: "Lexus", yearIssue: 2017, capLuggage: 570, isEngine: .drown, isWindow: .open, isLuggage: .medium)
 var truck1 = Car (type: .truck, mark: "Mitsubishi", yearIssue: 2010, capLuggage: 600, isEngine: .run, isWindow: .close, isLuggage: .empty)
 
-car1.changeWindow(valWind: car1.isWindow)
+car1.changeWindow(isWindow: car1.isWindow)
 // print(car1) - выводится не красиво
 
 print("\(car1.type.rawValue) - \(car1.mark) - \(car1.yearIssue) года выпуска - \(car1.capLuggage)л - \(car1.isEngine.rawValue) - \(car1.isWindow.rawValue) - \(car1.isLuggage.rawValue)")
