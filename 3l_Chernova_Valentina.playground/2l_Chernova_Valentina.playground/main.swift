9452cfe6ac58a8eb31cd586cbd5c679e0c9f3905
Lidiya666 <valeriya666@gmail.com>
2019-02-07 14:49:38 +0300

    изменения метода в третьем уроке

--- 3l_Chernova_Valentina.playground/2l_Chernova_Valentina.playground/main.swift
+++ 3l_Chernova_Valentina.playground/2l_Chernova_Valentina.playground/main.swift
@@ -28,11 +28,11 @@ struct Car {
     var isWindow: Window
     var isLuggage: Luggage
     
-    mutating func changeWindow(){
-        if isWindow == Window.close {
-            isWindow = Window.open
+    mutating func changeWindow(valWind: Window){
+        if valWind == Window.close {
+            self.isWindow = Window.open
         } else {
-            isWindow = Window.close
+            self.isWindow = Window.close
         }
     }
 }
@@ -41,9 +41,7 @@ var car1 = Car (type: .car, mark: "Hyundai", yearIssue: 2015, capLuggage: 510, i
 var car2 = Car (type: .car, mark: "Lexus", yearIssue: 2017, capLuggage: 570, isEngine: .drown, isWindow: .open, isLuggage: .medium)
 var truck1 = Car (type: .truck, mark: "Mitsubishi", yearIssue: 2010, capLuggage: 600, isEngine: .run, isWindow: .close, isLuggage: .empty)
 
-car1.changeWindow()
-car2.changeWindow()
-
+car1.changeWindow(valWind: car1.isWindow)
 // print(car1) - выводится не красиво
 
 print("\(car1.type.rawValue) - \(car1.mark) - \(car1.yearIssue) года выпуска - \(car1.capLuggage)л - \(car1.isEngine.rawValue) - \(car1.isWindow.rawValue) - \(car1.isLuggage.rawValue)")
