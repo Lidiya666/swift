6715f83fc6cb253f1e121c58148e508bdfffe4fd
Lidiya666 <valeriya666@gmail.com>
2019-02-13 18:58:10 +0300

    "основы swift" - 5 урок

---
+++ 5l_ChernovaValentina/5l_ChernovaValentina/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  5l_ChernovaValentina
+//
+//  Created by Lidiya on 13.02.2019.
+//  Copyright © 2019 lidiya. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
