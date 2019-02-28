d95678b6c831495243d86d502d57b83d3bf91b92
Lidiya666 <valeriya666@gmail.com>
2019-02-28 16:09:00 +0300

    интерфейс иос 1 урок

--- 1l_ChernovaVV/1l_ChernovaVV/LoginViewController.swift
+++ 1l_ChernovaVV/1l_ChernovaVV/LoginViewController.swift
@@ -1,21 +1,83 @@
-//
-//  LoginViewController.swift
-//  1l_ChernovaVV
-//
-//  Created by Lidiya on 28.02.2019.
-//  Copyright © 2019 lidiya. All rights reserved.
-//
-
 import UIKit
 
-class LoginViewController: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        
+        //присваиваем его UIScrollView
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
     }
-    */
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        // Получаем текст логина
+        let login = loginInput.text!
+        // Получаем текст-пароль
+        let password = passwordInput.text!
+        
+        // Проверяем, верны ли они
+        if login == "admin" && password == "123456" {
+            print("успешная авторизация")
+        } else {
+            print("неуспешная авторизация")
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //Когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+        
+        //Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets (top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        //Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+        
+    }
+    
+    //Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden (notification: Notification) {
+        
+        //Устанавливаем отступ внизу UIScrollView, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    //Подписка на уведомления от клавиатуры
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Подписываемся на два уведомления: одно приходит при повлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        //Второе - когда она пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    //Отписка
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
 
 }
