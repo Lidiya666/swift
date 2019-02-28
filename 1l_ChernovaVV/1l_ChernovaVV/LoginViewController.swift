6d5ec62f57e02184617f082bec5e3c02bc65562a
Lidiya666 <valeriya666@gmail.com>
2019-02-28 16:08:48 +0300

    интерфейс иос 1 урок

---
+++ 1l_ChernovaVV/1l_ChernovaVV/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  1l_ChernovaVV
+//
+//  Created by Lidiya on 28.02.2019.
+//  Copyright © 2019 lidiya. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ 1l_ChernovaVV/1l_ChernovaVVTests/_l_ChernovaVVTests.swift
@@ -0,0 +1,34 @@
+//
+//  _l_ChernovaVVTests.swift
+//  1l_ChernovaVVTests
+//
+//  Created by Lidiya on 28.02.2019.
+//  Copyright © 2019 lidiya. All rights reserved.
+//
+
+import XCTest
+@testable import _l_ChernovaVV
+
+class _l_ChernovaVVTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
